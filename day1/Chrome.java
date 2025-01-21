c9715290de77fb4f0ff579fdc15857f3c417f2ed
Auro4037 <104313271+Auro4037@users.noreply.github.com>
2025-01-20 23:49:47 -0500

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion=91.0f;
+		String browserType="Google";
+		boolean betaType=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println("The browser version is- " +browserVersion);
+		System.out.println("The browser type is- " +browserType);
+		System.out.println("The beta type is- " +betaType);
+		System.out.println("The release year was- " +releaseYear);
+		System.out.println("The shortcut key is- " +shortcutKey);
+		
+		
+
+	}
+
+}
